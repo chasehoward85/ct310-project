82a04fc50c2888c7981255e7ab27f5ae8883d930
chasehoward85 <chasehoward85@gmail.com>
2020-03-25 19:05:00 -0600

    About us

---
+++ aboutus.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<link rel="stylesheet" href="./aboutus.css" type="text/css">
+		</head>
+		
+		<main>
+			<body>
+				<div class="chase">
+					<a href=""><img src="./chase.jpg" alt="Chase"></a>
+					<h3>Chase Howard</h3>
+					<p>A sophomore majoring in Computer Science</p>
+				</div>
+				<br>
+				
+				<div class="frenchy">
+					<a href=""><img src="" alt="Frenchy"></a>
+					<h3>Name</h3>
+					<p>Bio</p>
+				</div>
+				<br>
+				
+				<div class="michael">
+					<a href=""><img src="" alt="Michael"></a>
+					<h3>Name</h3>
+					<p>Bio</p>
+				</div>
+			</body>
+			
+			<footer>
+				
+			</footer>
+		</main>
+	</html>
